ab1a0884bf0f9dfbb740ae4a59ef075a8f6929e5
O46002060 <81305029+O46002060@users.noreply.github.com>
2021-04-12 12:59:33 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,56 @@
+const contents = [
+    {
+        chirurgo: 'Andronaco L.',
+        immagine: 'c0.jpg',   
+        mail: 'e-mail: adrn23@os.com',
+        tipo: 'ortopedico'
+    },
+    {
+        chirurgo: 'Di Marco B.',
+        immagine: 'c0.jpg',  
+        mail:  'e-mail: dima1@os.com',
+        tipo: 'cardiologo'
+    },
+    {
+        chirurgo: 'Frede R.',
+        immagine: 'c0.jpg',  
+        mail: 'e-mail: rfre3@os.com',
+        tipo: 'otorinolaringoiatra'
+    },
+    {
+        chirurgo: 'Mari E.',
+        immagine: 'c0.jpg',   
+        mail:  'e-mail: ema23@osp2.com',
+        tipo: 'neurochirurgo'
+    },
+    {
+        chirurgo: 'Vince P.',
+        immagine: 'c0.jpg',  
+        mail:  'e-mail: pvinc3@os.com',
+        tipo: 'neurochirurgo'
+    },
+    {
+        chirurgo: 'Vinci A.',
+        immagine: 'c0.jpg',    
+        mail:  'e-mail: avinci223@os.com',
+        tipo: 'neurochirurgo'
+    },
+    {
+        chirurgo: 'Vinci S.',
+        immagine: 'c0.jpg',  
+        mail:  'e-mail: svnc2@os.com',
+        tipo: 'cardiologo'
+    },
+    {
+        chirurgo: 'Zase D.',
+        immagine: 'c0.jpg',   
+        mail:  'e-mail: dazas45@os.com',
+        tipo: 'cardiologo'
+    },
+    {
+        chirurgo: 'Drase D.',
+        immagine: 'c0.jpg',  
+        mail:  'e-mail: dadra5@os.com',
+        tipo: 'ortopedico'
+    }
+]
